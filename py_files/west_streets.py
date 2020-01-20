692c365f4ebb04c4aef875bb746ab9357cf98b28
Kelly Stroh <48226962+kellystroh@users.noreply.github.com>
2020-01-19 21:03:50 -0800

    first commit

---
+++ py_files/west_streets.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import glob
+import re
+
+path = r'/Users/Kelly/galvanize/projects/circles/MLK/west/us'
+files = glob.glob(path + '/*' + "/*.csv")
+states = []
+cities = []
+for x in files:
+    x = re.sub('/Users/Kelly/galvanize/projects/circles/MLK/west/us/', '', x)
+    x = re.sub('/.*', '', x)
+    states.append(x)
+
+for x in files:
+    x = re.sub('/Users/Kelly/galvanize/projects/circles/MLK/west/us/.*/', '', x)
+    x = re.sub('.csv', '', x)
+    cities.append(x)
+
+luther_df_west = pd.DataFrame(columns=['LON', 'LAT', 'NUMBER', 'STREET', 'UNIT', 'CITY', 'DISTRICT', 'REGION',
+       'POSTCODE', 'ID', 'HASH'])
+
+for csv in range(len(files)):
+    if csv % 50 == 0:
+        print(csv)
+    x = pd.read_csv(files[csv], dtype={'LON':'float64', 'LAT':'float64', 'NUMBER':'str', 'STREET':'str', 'UNIT':'str', 'CITY':'str', 'DISTRICT':'str', 'REGION':'str',
+       'POSTCODE':'str', 'ID':'str', 'HASH':'str'})
+    x.drop(['NUMBER', 'UNIT', 'ID', 'HASH'], axis=1)
+    if len(x) == len(x[x.CITY.isna()]):
+        x['CITY'] = cities[csv]
+        print('woo')
+    x['REGION'] = states[csv]
+    x = x[~x.STREET.isna()]
+    x.drop_duplicates(subset='STREET', keep='first', inplace = True)
+    x['STREET'] = x.STREET.str.lower()
+    new = x[x.STREET.str.contains("luther")]
+    new2 = x[x.STREET.str.contains("mlk")]
+    luther_df_west = pd.concat([luther_df_west, new, new2], axis=0, ignore_index=True)
+luther_df_west.to_csv('luther_df_west.csv')
